5c6a13d3a0543275be3fe7ffff66a1174624c4e1
viviana <dmpviviana@gmail.com>
2018-09-30 18:15:42 -0300

    arreglos

--- src/Componentes/HabilidadesT/HabilidaesT.js
+++ src/Componentes/HabilidadesT/HabilidaesT.js
@@ -1,12 +1,21 @@
 
 import  React from 'react';
-import {Grid,Typography} from '@material-ui/core';
+import {Grid,Typography, Hidden} from '@material-ui/core';
 
 const HabilidadesT = () => {
     return(
-        <Grid item xs={12} sm={9} md={7} >
-           <Typography > Habilidades</Typography>
+         <Grid>
+        <Hidden only={['xs']}>
+       
+        <Grid container justify="center" className="desktopMore" >
+        <Grid item xs={12} sm={12} md={12} xl={12} lg={12} style={{paddingTop: 75}}>
+           <Typography align="center" style={{color: 'white'}}> Habilidades</Typography>
         </Grid>
+        </Grid>
+       
+        </Hidden>
+        </Grid>
+        
     )
 }
 export default HabilidadesT;
--- src/Componentes/Proyectos/Proyectos.js
+++ src/Componentes/Proyectos/Proyectos.js
@@ -1,12 +1,21 @@
 
 import  React from 'react';
-import {Grid,Typography} from '@material-ui/core';
+import {Grid,Typography, Hidden} from '@material-ui/core';
 
 const Proyectos = () => {
     return(
-        <Grid item xs={12} sm={9} md={7}>
-           <Typography > Proyectos</Typography>
+         <Grid>
+        <Hidden only={['xs']}>
+       
+        <Grid container justify="center" className="desktopMore" >
+        <Grid item xs={12} sm={12} md={12} xl={12} lg={12} style={{paddingTop: 75}}>
+           <Typography align="center" style={{color: 'white'}}> Proyectos</Typography>
         </Grid>
+        </Grid>
+       
+        </Hidden>
+        </Grid>
+        
     )
 }
 export default Proyectos;

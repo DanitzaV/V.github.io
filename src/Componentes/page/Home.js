460f577195a44992088cdde6cc521dde9157e9a6
viviana <dmpviviana@gmail.com>
2018-09-30 16:36:20 -0300

    arreglo en home

--- src/Componentes/page/Home.js
+++ src/Componentes/page/Home.js
@@ -22,14 +22,40 @@ class Home extends Component {
           </Grid>
          
         </Hidden>
-        <Hidden only={['xs']} >
+        <Hidden only={['xs','md']} >
           <Grid container justify="center" className="desktop" >
           <Grid item sm={6} md={7} xl={8} style={{paddingTop: 40,}} className={['fondo']}>
             <h2 className="title">Viviana Marcelo</h2>
             <p  className="aboutme">Front-end Developer</p>
+            <Typography align="center" style={{ color: 'white' }}>Cuando salí de la secundaria , no sabía qué estudiar,
+              afortunadamente Laboratoria se presentó en mi camino.En el bootcamp
+              descubrí que me encanta codear y aprendí todo lo necesario para convertirme en
+              front-end developer.
+            </Typography>
           </Grid>
           <Grid item sm={6} md={5} xl={4} style={{paddingTop: 180,paddingLeft: 70,paddingRight: 70}} className="fondo">    
             <Typography align="center" style={{ color: 'white' }}>Cuando salí de la secundaria , no sabía qué estudiar,
+              afortunadamente Laboratoria se presentó en mi camino.En el bootcamp
+              descubrí que me encanta codear y aprendí todo lo necesario para convertirme en
+              front-end developer.
+            </Typography>
+          </Grid>
+          </Grid>
+          
+        </Hidden>
+        <Hidden only={['xs','sm','lg','xl']} >
+          <Grid container justify="center" className="desktop" >
+          <Grid item sm={6} md={7} xl={8} style={{paddingTop: 40,}} className={['fondo']}>
+            <h2 className="title">Viviana Marcelo </h2>
+            <p  className="aboutme">Front-end Developer</p>
+            <Typography align="center" className="parrafoMd" style={{ color: 'white' }} style={{paddingLeft: 61,paddingRight: 74,color: 'white'}}>Cuando salí de la secundaria , no sabía qué estudiar,
+              afortunadamente Laboratoria se presentó en mi camino.En el bootcamp
+              descubrí que me encanta codear y aprendí todo lo necesario para convertirme en
+              front-end developer.
+            </Typography>
+          </Grid>
+          <Grid item sm={6} md={5} xl={4} style={{paddingTop: 180,paddingLeft: 61,paddingRight: 74}} className="fondo">    
+          <Typography align="center" className="parrafoMd" style={{ color: 'white' }}>Cuando salí de la secundaria , no sabía qué estudiar,
               afortunadamente Laboratoria se presentó en mi camino.En el bootcamp
               descubrí que me encanta codear y aprendí todo lo necesario para convertirme en
               front-end developer.
